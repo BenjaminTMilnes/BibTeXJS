3b5f9696925586468d3ee7494aa39483a1f55003
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-08 21:36:11 +0000

    Added the base classes BibTeXField and BibTeXEntry, as well as a simple version of BibTeXExporter

--- bibtex.js
+++ bibtex.js
@@ -1,17 +1,81 @@
 
-
-class BibTeXBook {
-    constructor() {
-        this.author = "";
-        this.editor = "";
-        this.title = "";
-        this.publisher = "";
-        this.year = "";
-        this.volume = "";
-        this.number = "";
-        this.series = "";
-        this.address = "";
-        this.edition = "";
-        this.month = "";
+class BibTeXEntry {
+    constructor(name) {
+        this.name = name;
+        this.citationKey = "";
+        this.note = new BibTeXField("note", "");
+        this.key = new BibTeXField("key", "");
     }
+}
+
+class BibTeXField {
+    constructor(name, value, isOptional = true, requiredFieldGroup = "") {
+        this.name = name;
+        this.value = value;
+        this.isOptional = isOptional;
+        this.requiredFieldGroup = requiredFieldGroup;
+    }
+}
+
+class BibTeXBook extends BibTeXEntry {
+    constructor() {
+        super.constructor("book");
+
+        this.author = new BibTeXField("author", "", true, "author/editor");
+        this.editor = new BibTeXField("editor", "", true, "author/editor");
+        this.title = new BibTeXField("title", "", false);
+        this.publisher = new BibTeXField("publisher", "", false);
+        this.year = new BibTeXField("year", "", false);
+        this.volume = new BibTeXField("volume", "");
+        this.number = new BibTeXField("number", "");
+        this.series = new BibTeXField("series", "");
+        this.address = new BibTeXField("address", "");
+        this.edition = new BibTeXField("edition", "");
+        this.month = new BibTeXField("month", "");
+    }
+}
+
+class BibTeXExporter {
+
+    getBibTeXEntryFields(entry) {
+        var fields = [];
+
+        for (var propertyName in entry){
+            var property = entry[propertyName];
+
+            if (property instanceof BibTeXField){
+                fields.push(property);
+            }
+        }
+
+        return fields;
+    }
+
+    convertBibTeXFieldToText(field) {
+        return  field.name + " = \"" + field.value + "\"";
+    }
+
+    convertBibTeXFieldsToText(fields){
+        var text = "";
+
+        for (var i =0; i < fields.length; i++){
+            var field = fields[i];
+
+            text += ",\n\t" + this.convertBibTeXFieldToText(field);
+        }
+
+        return text;
+    }
+
+    convertBibTeXEntryToText(entry) {
+        var fields = this.getBibTeXEntryFields(entry);
+        var text = "";
+
+        text += "@" + entry.name + "{" + entry.citationKey;
+        text += this.convertBibTeXFieldsToText(fields);
+        text += "}\n";
+
+        return text;
+    }
+
 }
