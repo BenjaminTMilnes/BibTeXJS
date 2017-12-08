30b7793feacfefc3673d437632f5e94fc22a45ce
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-08 20:48:45 +0000

    Added a basic class for BibTeXBook

---
+++ bibtex.js
@@ -0,0 +1,17 @@
+
+
+class BibTeXBook {
+    constructor() {
+        this.author = "";
+        this.editor = "";
+        this.title = "";
+        this.publisher = "";
+        this.year = "";
+        this.volume = "";
+        this.number = "";
+        this.series = "";
+        this.address = "";
+        this.edition = "";
+        this.month = "";
+    }
+}
