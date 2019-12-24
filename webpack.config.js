9f3011b1d5b3b55fb8fe2b19e6af0d2f1ffd7fa9
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-12-24 01:14:00 +0000

    Made build

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const glob = require("glob");
 var entry = { bibtex: "./source/bibtex.js" };
 var outputPath = "distribution";
 
-process.env.TESTBUILD = true;
+process.env.TESTBUILD = false;
 
-if (process.env.TESTBUILD) {
+if (process.env.TESTBUILD == true) {
     var entry = glob.sync(__dirname + "/tests/*.test.js");
     var outputPath = "tests-distribution";
 }
