e81f383f5ac083217fe0ea7c9d6ee5f21a329a5d
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-12-24 01:10:25 +0000

    Added more to importer unit tests; 7 tests; all passing

--- tests/bibtex_importer.test.js
+++ tests/bibtex_importer.test.js
@@ -8,14 +8,42 @@ var example1 = `
       author = "J. K. Rowling",
       title = "Harry Potter and the Philosopher's Stone"
   }
+  @book{HarryPotter2,
+      author = "J. K. Rowling",
+      title = "Harry Potter and the Chamber of Secrets"
+  }
+  
+  @book{TheLordOfTheRings,
+        author={J. R. R. Tolkien},
+        title={The Lord of the Rings}
+    }
   `;
-
-var database = importer.importDatabase(example1);
 
 describe("BibTeXImporter", function () {
 
-    it("", function () {
-        assert.equal(database.getEntryByCitationKey("HarryPotter1").author.value, "J. K. Rowling");
+    describe("importDatabase", function () {
+
+        it("should correctly import example 1.", function () {
+            var database1 = importer.importDatabase(example1);
+
+            assert.equal(database1.entries.length, 3);
+
+            var entry1 = database1.getEntryByCitationKey("HarryPotter1");
+
+            assert.equal(entry1.author.value, "J. K. Rowling");
+            assert.equal(entry1.title.value, "Harry Potter and the Philosopher's Stone");
+
+            var entry2 = database1.getEntryByCitationKey("HarryPotter2");
+
+            assert.equal(entry2.author.value, "J. K. Rowling");
+            assert.equal(entry2.title.value, "Harry Potter and the Chamber of Secrets");
+
+            var entry3 = database1.getEntryByCitationKey("TheLordOfTheRings");
+
+            assert.equal(entry3.author.value, "J. R. R. Tolkien");
+            assert.equal(entry3.title.value, "The Lord of the Rings");
+        });
+
     });
 
 });
